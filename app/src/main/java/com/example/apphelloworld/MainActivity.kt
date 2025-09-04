cab31da564354ec8b36aaa3cf2035df40f1b6ddf
2Bmodular <2bmod@bglab.local>
2025-09-04 20:45:21 -0300

    orlando, marcos arão,fabiano,flavio

--- app/src/main/java/com/example/apphelloworld/MainActivity.kt
+++ app/src/main/java/com/example/apphelloworld/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.apphelloworld
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var email = findViewById<EditText>(R.id.editTextEmail)
+        var senha = findViewById<EditText>(R.id.editTextSenha)
+        var btnLogar = findViewById<Button>(R.id.buttonLogar)
+        var btnCadastrar = findViewById<Button>(R.id.buttonCadastrar)
+        btnLogar.setOnClickListener {
+
+            if(email.text.isEmpty()|| senha.text.isEmpty()){
+                Toast.makeText(this,"E-mail ou senha vazio", Toast.LENGTH_LONG).show()
+
+            }else{
+                // lógica para redirecionar para a tela de seguros.
+               var intent = Intent(this, SeguroActivity ::class.java)
+                startActivity(intent)
+            }
+        }
+btnCadastrar.setOnClickListener {
+    var intent = Intent(this, CadastroActivy::class.java)
+    startActivity (intent)
+}
     }
 }
